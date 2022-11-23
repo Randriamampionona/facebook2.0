b44d38e8d4e67e288817c3906b4da1cb89001974
Randriamampionona <105777560+Randriamampionona@users.noreply.github.com>
2022-11-23 19:04:40 +0300

    profile and config fixed

--- configs/mysqlBD.js
+++ configs/mysqlBD.js
@@ -1,19 +1,31 @@
 import mysql from "mysql2";
 
 const mysqlDB = mysql.createPool({
-	// host: process.env.NODE_ENV === "production" ? "db4free.net" : "localhost",
-	// user: process.env.NODE_ENV === "production" ? "myfacebook" : "root",
-	// password: process.env.NODE_ENV === "production" ? "12345678910" : "",
-	// database:
-	// 	process.env.NODE_ENV === "production" ? "myfacebook" : "facebook2.0",
+	host:
+		process.env.NODE_ENV === "production"
+			? process.env.NEXT_HOST
+			: "localhost",
 
-	// testing
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "testbd",
+	user: process.env.NODE_ENV === "production" ? process.env.NEXT_USER : "root",
+
+	password:
+		process.env.NODE_ENV === "production" ? process.env.NEXT_PASSWORD : "",
+
+	database:
+		process.env.NODE_ENV === "production"
+			? process.env.NEXT_DATABASE
+			: "facebook2.0",
+
 	multipleStatements: true,
 	connectionLimit: Infinity,
+
+	// testing
+	// host: "localhost",
+	// user: "root",
+	// password: "",
+	// database: "testbd",
+	// multipleStatements: true,
+	// connectionLimit: Infinity,
 });
 
 export default mysqlDB;
